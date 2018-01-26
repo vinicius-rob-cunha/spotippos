b1b770af7d0ad3157457d9f283ee31efafb096d9
Vinicius <vinicius.cunha@b2wdigital.com>
2018-01-26 10:37:50 -0200

    Alterando o delete para ser async

--- spotippos-auth-server/src/main/java/br/com/spotippos/auth/repository/RefreshTokenRepository.java
+++ spotippos-auth-server/src/main/java/br/com/spotippos/auth/repository/RefreshTokenRepository.java
@@ -2,11 +2,13 @@ package br.com.spotippos.auth.repository;
 
 import br.com.spotippos.auth.model.AuthRefreshToken;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.scheduling.annotation.Async;
 
 import java.time.LocalDateTime;
 
 public interface RefreshTokenRepository extends CrudRepository<AuthRefreshToken, Integer> {
 
+    @Async
     void deleteByExpireAtLessThan(LocalDateTime now);
 
 }
